b48c35f04469f52d21ec91365e33c67deeda5a0c
BlueEagel777 <andrewleroux777@gmail.com>
2025-12-29 14:14:27 +0200

    2025: Day 1 part 1

--- 2025/Day1/main.cpp
+++ 2025/Day1/main.cpp
@@ -1,6 +1,45 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 int main() {
-    std::cout << "Hello World!";
+
+    int safeDial = 50;
+    unsigned int password = 0;
+
+    std::cout << "Reading the text file" << std::endl;
+    std::ifstream inputFile("input.txt");
+    if (inputFile.is_open()) {
+        std::string line;
+        while (std::getline(inputFile, line)) {
+            char rotateDirection = line[0];
+            int rotationNum = std::stoi(line.substr(1));
+
+            // Rotate the dial
+            if (rotateDirection == 'R') {
+                safeDial += rotationNum;
+                while (safeDial > 99) {
+                    safeDial -= 100;
+                }
+            } else if (rotateDirection == 'L') {
+                safeDial -= rotationNum;
+                while (safeDial < 0) {
+                    safeDial += 100;
+                }
+            }
+
+            std::cout << safeDial << std::endl;
+
+            // Get the password
+            if (safeDial == 0) {
+                password++;
+            }
+        }
+        inputFile.close();
+    } else {
+        std::cout << "Error: Unable to open the file." << std::endl;
+    }
+
+    std::cout << "The password is: " << password;
     std::cout << std::endl;
 }
