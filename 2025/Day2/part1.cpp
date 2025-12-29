15d50b325ca760852e6a66754e27f9474f09eb61
BlueEagel777 <andrewleroux777@gmail.com>
2025-12-29 18:20:54 +0200

    2025: day 2 part 1 read data done

--- 2025/Day2/part1.cpp
+++ 2025/Day2/part1.cpp
@@ -1,6 +1,53 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+struct range {
+    unsigned int start;
+    unsigned int end;
+};
 
 int main() {
-    std::cout << "Hello Part 1!";
-    std::cout << std::endl;
+    std::ifstream inputFile("input.txt");
+    std::string fileContents;
+    std::vector<range> rangeArray;
+
+    // Read the data out of the file
+    if (inputFile.is_open()) {
+        std::string line;
+        while (std::getline(inputFile, line)) {
+            fileContents.append(line);
+        }
+        inputFile.close();
+    } else {
+        std::cout << "Error: Unable to open the file." << std::endl;
+    }
+
+    std::cout << fileContents << std::endl;
+
+    //Read the data into an vector
+    bool stopRangeArrayGen = false;
+    do {
+        int hyphenLoc = fileContents.find('-');
+        int commaLoc = fileContents.find(',');
+        range rangeToAdd;
+
+        rangeToAdd.start = std::stoi(fileContents.substr(0,hyphenLoc));
+        rangeToAdd.end = std::stoi(fileContents.substr(hyphenLoc + 1, commaLoc));
+        rangeArray.push_back(rangeToAdd);
+
+        //Check if the end has been reached
+        if (commaLoc == -1 ) {
+            stopRangeArrayGen = true;
+        } else {
+            fileContents.erase(0,commaLoc+1);
+        }
+    } while (stopRangeArrayGen == false);
+
+
+    for (range inspected :  rangeArray) {
+        std::cout << inspected.start << " " << inspected.end << std::endl;
+    }
+
 }
