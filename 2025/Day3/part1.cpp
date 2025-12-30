6e60d55fcf20ffee1ebec2f618eee70eeeff4b88
BlueEagel777 <andrewleroux777@gmail.com>
2025-12-30 14:20:14 +0200

    2025: Day 3 part 1

--- 2025/Day3/part1.cpp
+++ 2025/Day3/part1.cpp
@@ -1,6 +1,60 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <string>
+
+unsigned int getHighestDigitInRange(const std::string& batteryBank, unsigned int startPos, unsigned int endPos) {
+    int highestValPos = -1;
+    int highestVal = -1;
+    for (unsigned int i = startPos; i < endPos; i++) {
+        int inspectedBatteryVoltage = batteryBank[i] - '0'; // Convert char to int
+        if (highestVal< inspectedBatteryVoltage) {
+            highestValPos = i;
+            highestVal = inspectedBatteryVoltage;
+        }
+    }
+    return highestValPos;
+}
+
+unsigned int concatVoltages(const std::string& batteryBank, unsigned int leftDigitPos, unsigned int rightDigitPos) {
+    return std::stoi(std::string(1,batteryBank[leftDigitPos]) + batteryBank[rightDigitPos]);
+}
+
+unsigned int getBankVoltage(const std::string& batteryBank) {
+
+    unsigned int highestBatVoltPos =
+        getHighestDigitInRange(batteryBank, 0, batteryBank.length());
+
+    int leftHighestBatVoltPos = -1;
+    if (highestBatVoltPos > 0) {
+        leftHighestBatVoltPos = getHighestDigitInRange(batteryBank, 0, highestBatVoltPos);
+    }
+
+    int rightHighestBatVoltPos = -1;
+    if (highestBatVoltPos < (batteryBank.length() - 1) ) {
+        rightHighestBatVoltPos = getHighestDigitInRange(batteryBank, highestBatVoltPos + 1, batteryBank.length());
+    }
+
+    //See if either number is automatically disqualified
+    if (leftHighestBatVoltPos == -1) {
+        return concatVoltages(batteryBank, highestBatVoltPos, rightHighestBatVoltPos);
+    } else if (rightHighestBatVoltPos == -1) {
+        return concatVoltages(batteryBank, leftHighestBatVoltPos, highestBatVoltPos);
+    } else {
+        // Both numbers are possible candidates
+        // Get the two numbers to compare
+        unsigned int rightNumber =
+            concatVoltages(batteryBank, leftHighestBatVoltPos, highestBatVoltPos);
+        unsigned int leftNumber =
+            concatVoltages(batteryBank, highestBatVoltPos, rightHighestBatVoltPos);
+        // Provide the largest number that has been found
+        if (leftNumber > rightNumber) {
+            return leftNumber;
+        } else {
+            return rightNumber;
+        }
+    }
+}
 
 int main() {
     //Storage variables
@@ -18,8 +72,15 @@ int main() {
         std::cout << "Error: Unable to open the file." << std::endl;
     }
 
-    for (std::string batteryBank :batteryBanks) {
-        std::cout << batteryBank << "\n";
+    std::vector<unsigned int> bankVoltages;
+    for (const std::string& batteryBank : batteryBanks) {
+        bankVoltages.push_back(getBankVoltage(batteryBank));
     }
-    std::cout << std::endl;
+
+    //Gather the sum
+    unsigned int totalVoltage = 0;
+    for (unsigned int bankVoltage : bankVoltages) {
+        totalVoltage += bankVoltage;
+    }
+    std::cout << "Total voltage of banks is:  " << totalVoltage << std::endl;
 }
